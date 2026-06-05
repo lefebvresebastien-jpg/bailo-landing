75d12272a3d0ba53de93ceb80120f917be9efd7b
lefebvresebastien-jpg <lefebvresebastien@yahoo.fr>
2026-06-05 08:26:35 +0200

    fix: LinkedIn-Version 202306

--- netlify/functions/linkedin-post.js
+++ netlify/functions/linkedin-post.js
@@ -36,7 +36,7 @@ exports.handler = async function(event) {
       headers: {
         'Authorization': `Bearer ${linkedinToken}`,
         'Content-Type': 'application/json',
-        'LinkedIn-Version': '202501',
+        'LinkedIn-Version': '202306',
         'X-Restli-Protocol-Version': '2.0.0'
       },
       body: JSON.stringify({
